661e1e8e746486fcaf6edfaf9b2250288dc258da
Szabo Bence (CC-DA/EAR1-Bp) <BENCE.SZABO@hu.bosch.com>
2019-05-22 16:34:50 +0200

    compiles, runs

---
+++ Makefile
@@ -0,0 +1,30 @@
+#choose wisely
+GXX  := g++
+CLXX := clang++
+CXX  := $(CLXX)
+
+ifeq ($(CXX),clang++)
+  MINGW_MODE := --target=x86_64-pc-windows-gnu  -fuse-ld=lld
+endif
+
+CXXFLAGS   := -std=c++17 -O2 -g -Wall -Wextra -Werror
+BOOSTLIBS  := -lboost_system-mt
+WINSOCKLIBS:= -lwsock32 -lws2_32
+
+HOST_LLVM := c:\llvm8
+BOOST     := $(HOST_LLVM)\include\boost
+
+
+
+all: server.exe client.exe
+
+clean:
+	rm -rf $(CURDIR)/*.exe
+	rm -rf $(CURDIR)/*.pch
+
+%.exe: %.cpp asio.hpp.pch Makefile
+	$(CXX) $< -o $@ $(CXXFLAGS) -I$(BOOST) $(BOOSTLIBS) $(WINSOCKLIBS) $(MINGW_MODE) -include-pch asio.hpp.pch
+
+
+asio.hpp.pch: $(BOOST)\asio.hpp Makefile
+	$(CXX) $< -o $@ $(CXXFLAGS) -I$(BOOST) $(MINGW_MODE)
---
+++ server.cpp
@@ -0,0 +1,55 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+int main()
+{  
+  try
+  { 
+    const unsigned int port = 3632; //port the server listen
+    const unsigned int buff_size = 16384; //size of the send buffer
+
+    boost::asio::io_service io_service; //main asio object
+    tcp::endpoint endpoint(tcp::v4(), port); //endpoint
+    tcp::acceptor acceptor(io_service, endpoint);  //we accept connection there
+
+    //server loop
+    while(1) {
+      tcp::socket socket(io_service);  //create a socket
+      acceptor.accept(socket); //attach to the acceptor
+      //we have got a new connection !
+      std::cout << " Remote @:port  " << socket.remote_endpoint().address() << " : "
+		<< socket.remote_endpoint().port() << std::endl;
+
+      std::fstream file("client.cpp"); //we open this file
+
+      char* buff = new char[buff_size]; //creating the buffer
+      unsigned int count = 0; //counter
+      std::cout << "Sending" << std::endl;
+      while( !file.eof() ) { //loop until there is no more data to send
+        memset(buff,0,buff_size); //cleanup the buffer
+        file.read(buff,buff_size); //read some data 
+        boost::system::error_code ignored_error;
+        unsigned int len = file.gcount(); //get the effective number of bytes read 
+        boost::asio::write(socket, boost::asio::buffer(buff,len),
+                boost::asio::transfer_all(), ignored_error); //send
+        count+=len; //increment counter
+      }
+
+      file.close(); //close file
+      delete[] buff;  //delete buffer 
+      std::cout << "Finished" << std::endl;
+      std::cout << "Sent "  << count << " bytes" << std::endl;
+    }
+  
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}

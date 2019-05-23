41a9d3aafe62168919148a643f50398e63bc7933
Szabo Bence (CC-DA/EAR1-Bp) <BENCE.SZABO@hu.bosch.com>
2019-05-23 17:06:57 +0200

    can send and recive ony once

--- client.cpp
+++ client.cpp
@@ -10,42 +10,30 @@ int main(int argc, char *argv[]) {
   try {
 
     const char *port = "3632";            // the port we connect
-    const unsigned int buff_size = 65536; // the size of the read buffer
+    const char *server = "10.76.153.34";
+    // const unsigned int buff_size = 65536; // the size of the read buffer
 
-    if (argc != 3) {
-      std::cerr << "Usage: file_client <host> <filename>" << std::endl;
-      return 1;
-    }
+    std::cout << "Usage: type text and press enter to send." << std::endl;
+
     boost::asio::io_service io_service;        // asio main object
     tcp::resolver resolver(io_service);        // a resolver for name to @
-    tcp::resolver::query query(argv[1], port); // ask the dns for this resolver
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(
-        query); // iterator if multiple answers for a given name
+    tcp::resolver::query (server, port); // ask the dns for this resolver
+    tcp::resolver::iterator endpoint = resolver.resolve(tcp::resolver::query (server, port)); // iterator if multiple answers for a given name
     tcp::resolver::iterator end;
 
     tcp::socket socket(io_service); // attach a socket to the main asio object
-    socket.connect(*endpoint_iterator); // connect to the first returned object
+    socket.connect(*endpoint); // connect to the first returned object
+    // boost::system::error_code error;
+    // unsigned int count = 0;              // a counter
+    while (1) {         // loop until there is no more data to send
+      std::string msg = "";
+      std::cin >> msg;
+      size_t len = socket.send(boost::asio::buffer(msg));
+      std::cout << "Sent '" << msg << "'("<< len << "bytes)" << std::endl;
 
-    std::fstream f(argv[2],
-                   std::fstream::out);   // open in write mode the output file
-    unsigned int count = 0;              // a counter
-    while (1) {                          // loop until (see break after)
-      boost::array<char, buff_size> buf; // create read buffer
-      boost::system::error_code error;   // in case of error
-      size_t len =
-          socket.read_some(boost::asio::buffer(buf), error); // read data
-      std::cout << "Read " << len << std::endl;
-      count += len;
-      std::cout << "Read a total of " << count << " bytes " << std::endl;
-      if (error == boost::asio::error::eof) { // if end of file reached
-        f.write(buf.data(), len);             // finish write data
-        f.close();
-        break; // Connection closed cleanly by peer.
-      } else if (error) {
-        throw boost::system::system_error(error); // Some other error.
-      } else {
-        f.write(buf.data(), len); // write some data
-      }
+      msg = ""; // create read buffer
+      len = socket.receive(boost::asio::buffer(msg)); // read data
+      std::cout << "Got '" << msg << "' (" << len << "bytes)" << std::endl;
     }
   } catch (std::exception &e) {
     std::cout << "Exception: " << e.what() << std::endl;
--- server.cpp
+++ server.cpp
@@ -1,15 +1,14 @@
-#include <boost/asio.hpp>
+#include <boost/asio.hpp> 
 #include <fstream>
 #include <iostream>
 #include <string>
-
 
 using boost::asio::ip::tcp;
 
 int main() {
   try {
     const unsigned int port = 3632;       // port the server listen
-    const unsigned int buff_size = 16384; // size of the send buffer
+    // const unsigned int buff_size = 16384; // size of the send buffer
 
     boost::asio::io_service io_service;           // main asio object
     tcp::endpoint endpoint(tcp::v4(), port);      // endpoint
@@ -22,32 +21,18 @@ int main() {
       // we have got a new connection !
       std::cout << " Remote @:port  " << socket.remote_endpoint().address()
                 << " : " << socket.remote_endpoint().port() << std::endl;
+      socket.wait(boost::asio::socket_base::wait_read);
+      std::string msg = ""; // create read buffer
+      size_t len = socket.receive(boost::asio::buffer(msg)); // read data
+      std::cout << "Got '" << msg << "' (" << len << "bytes)" << std::endl;
 
-      std::fstream file("client.cpp"); // we open this file
-
-      char *buff = new char[buff_size]; // creating the buffer
-      unsigned int count = 0;           // counter
-      std::cout << "Sending" << std::endl;
-      while (!file.eof()) {         // loop until there is no more data to send
-        memset(buff, 0, buff_size); // cleanup the buffer
-        file.read(buff, buff_size); // read some data
-        boost::system::error_code ignored_error;
-        unsigned int len =
-            file.gcount(); // get the effective number of bytes read
-        boost::asio::write(socket, boost::asio::buffer(buff, len),
-                           boost::asio::transfer_all(), ignored_error); // send
-        count += len; // increment counter
-      }
-
-      file.close();  // close file
-      delete[] buff; // delete buffer
-      std::cout << "Finished" << std::endl;
-      std::cout << "Sent " << count << " bytes" << std::endl;
+      len = socket.send(boost::asio::buffer("OK"));
+      std::cout << "Sent OK (" << len << " bytes)" << std::endl;
     }
-
   } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
   }
 
   return 0;
 }
+  
